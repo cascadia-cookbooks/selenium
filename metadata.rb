6a180a138570529a291ef5a334e7a6570edeb3cb
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-02 13:59:42 -0700

    0.0.0

---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'cop_selenium'
+maintainer 'Copious Inc.'
+maintainer_email 'engineering@copiousinc.com'
+license 'MIT'
+description 'Installs and configures tools for testing.'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version '0.0.0'
+
+source_url 'https://github.com/copious-cookbooks/selenium'
+issues_url 'https://github.com/copious-cookbooks/selenium/issues'
+
+supports 'ubuntu', '>= 14.04'
+supports 'debian', '>= 8'
+supports 'rhel', '>= 6'
+supports 'centos', '>= 6'
