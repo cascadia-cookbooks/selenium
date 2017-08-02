9b00499c9bf77a005cf71f882fa5de840d9aaf61
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-02 14:46:23 -0700

    Test for OpenJDK package installation

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,4 +1,15 @@
 require 'spec_helper'
 
+case os[:family]
+when 'debian', 'ubuntu'
+    pkg = 'openjdk-8-jre'
+when 'redhat'
+    pkg = 'java-1.8.0-openjdk'
+end
+
 describe 'selenium::default' do
+    describe package(pkg) do
+        it { should be_installed }
+    end
+
 end
