1baceef15f93c0ac79cfcab7b5916d8883126d0e
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-02 15:31:51 -0700

    Attributes for Selenium .jar

---
+++ attributes/default.rb
@@ -0,0 +1,4 @@
+default['selenium']['version'] = '3.4.0'
+default['selenium']['path']    = '/opt/selenium-server-standalone.jar'
+default['selenium']['owner']   = 'root'
+default['selenium']['group']   = 'root'
