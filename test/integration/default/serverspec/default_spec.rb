2cb1b887a8a26ec36fba44924c60b70f3397d7e8
Jerry Wardlow <jerry@jerryoftheyear.com>
2017-08-03 09:35:58 -0700

    Further logic to break down pkg by OS release version

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,10 +1,16 @@
 require 'spec_helper'
 
+# Check which OS family the node belongs to (Red Hat, Debian)
 case os[:family]
-when 'debian', 'ubuntu'
-    pkg = 'openjdk-8-jre'
 when 'redhat'
     pkg = 'java-1.8.0-openjdk'
+when 'debian', 'ubuntu'
+    case os[:release].to_i # Drop OS release minor version (no decimal)
+    when 14, 8 # Ubuntu Trusty, Debian Jessie
+        pkg = 'openjdk-7-jre'
+    when 16, 9 # Ubuntu Xenial, Debian Stretch
+        pkg = 'openjdk-8-jre'
+    end
 end
 
 describe 'selenium::default' do
